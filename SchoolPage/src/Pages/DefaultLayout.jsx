e85e1def4a1b0ff018663cfb654c6916438c91b4
khoa <khoalun@gmail.com>
2025-10-25 09:37:34 +0700

    add

---
+++ SchoolPage/src/Pages/DefaultLayout.jsx
@@ -0,0 +1,13 @@
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
+
+function DefaultLayout() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default DefaultLayout;
