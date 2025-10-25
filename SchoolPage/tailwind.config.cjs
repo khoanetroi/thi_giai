226f03f696694c554dbd73b23782031ed055e93b
qdung030404 <qdung030404@gmail.com>
2025-10-25 09:42:44 +0700

    .

--- SchoolPage/tailwind.config.cjs
+++ SchoolPage/tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,html}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "node_modules/flowbite-react/lib/esm/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
