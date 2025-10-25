0c84ebaf5923c258a27330ed857af17bcdfd7f3e
qdung030404 <qdung030404@gmail.com>
2025-10-25 09:58:42 +0700

    hedaer

--- SchoolPage/src/App.jsx
+++ SchoolPage/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import Footer   from "./Pages/Layout/footer.jsx";
+import Header from "./Pages/Layout/Header.jsx";
 function App() {
 
 
   return (
     <>
+        <Header />
       <Footer/>
     </>
   )
